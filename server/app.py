f60b88d9c23654e16606cb09463214cb3d214c86
Nikki Little <nikkilittle13@gmail.com>
2024-07-17 13:39:55 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -28,16 +28,33 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+    
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+
+        password = request.get_json()['password']
+
+        if user is not None:
+            session['user_id'] = user.id
+            return user.to_dict()
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
